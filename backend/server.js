451ab454164285639f7ae0d5b44375366ae92055
amitkumar875095 <amitkumar.ug21@nsut.ac.in>
2024-06-18 04:39:37 -0700

    add error handler and wrong url

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,16 @@ const userRoutes = require('./routes/userRoutes');
 dotenv.config();
 const chats = require('./data/data');
 const ConnectDB = require('./config/db');
+const {notFound,errorHandler} = require('./middleware/errorMiddleware');
+
 ConnectDB();
 app.use(cors());
 app.use(express.json());
 app.get('/',(req,res)=>{
     res.send("API is running");
 })
-
+app.use(notFound);
+app.use(errorHandler);
 app.use('/api/user',userRoutes)
 const PORT = process.env.PORT
 app.listen(PORT,console.log("Server Started at Port ",PORT));
