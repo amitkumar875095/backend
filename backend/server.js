f602b85c81740539826370b2613c03ff7fc38913
amitkumar875095 <amitkumar.ug21@nsut.ac.in>
2024-07-03 15:32:25 -0700

    add socket io

--- backend/server.js
+++ backend/server.js
@@ -30,9 +30,35 @@ const server = app.listen(PORT,console.log("Server Started at Port ",PORT));
 const io = require('socket.io')(server,{
     pingTimeout : 60000,
     cors : {
-        origin : "http://localhost3000",
+        origin : "http://localhost:3000",
     },
 });
 io.on("connection",(socket)=>{
-    console.log("connected to socket io")
+    console.log("connected to socket io");
+
+     socket.on('setup',(userData)=>{
+           socket.join(userData._id);
+           console.log("userData",userData._id);
+           socket.emit('connected');
+     })
+     socket.on("join Chat",(room)=>{
+        socket.join(room);
+        console.log("User joined Room : "+room);
+    })
+  socket.on("new message",(newMessageReceived)=>{
+    var chat = newMessageReceived.chat;
+    if(!chat.users) return console.log("chats users not defined");
+
+    chat.users.forEach(user=>{
+        if(user._id == newMessageReceived.sender._id) return;
+
+        socket.in(user._id).emit("message received",newMessageReceived);
+    })
+  })
+
+   socket.on('typing',(room)=>socket.in(room).emit("typing"));
+   socket.on('stop typing',(room)=>{
+      
+    socket.in(room).emit("stop typing")});
 });
+
